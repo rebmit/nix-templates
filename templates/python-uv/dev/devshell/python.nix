8ddf65ebf6774c2a00251b69216e0571da6ea50b
Lu Wang <rebmit@rebmit.moe>
2025-11-27 02:15:06 +0800

    python-uv: scope LD_LIBRARY_PATH to python to avoid breaking other tools

--- templates/python-uv/dev/devshell/python.nix
+++ templates/python-uv/dev/devshell/python.nix
@@ -1,26 +1,31 @@
+# https://github.com/pyproject-nix/pyproject.nix/blob/d6c61dbe0be75e2f4cf0efcdc62428175be4cfb5/templates/impure/flake.nix
+# https://github.com/cachix/devenv/blob/95d531cff33ce91cb028e3659f616c3ca3e99073/src/modules/languages/python/default.nix
 {
   perSystem =
     { lib, pkgs, ... }:
+    let
+      libraries = pkgs.pythonManylinuxPackages.manylinux1 ++ [ pkgs.stdenv.cc.cc.lib ];
+
+      makeWrapperArgs = [
+        "--prefix"
+        "LD_LIBRARY_PATH"
+        ":"
+        (lib.makeLibraryPath libraries)
+      ];
+    in
     {
       devshells.default = {
         commands = map (cmd: cmd // { category = "python"; }) [
-          { package = pkgs.python3; }
+          {
+            package = pkgs.python3.buildEnv.override (args: {
+              inherit makeWrapperArgs;
+            });
+          }
           { package = pkgs.uv; }
           { package = pkgs.ty; }
         ];
-        env = [
-          {
-            name = "PYTHONPATH";
-            unset = true;
-          }
-        ]
-        ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-          {
-            name = "LD_LIBRARY_PATH";
-            value = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
-          }
-        ];
         devshell.startup.uv.text = ''
+          unset PYTHONPATH
           uv sync
           . .venv/bin/activate
         '';
